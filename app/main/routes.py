c2617de9cd32af7434fd5349f46daa862b058fea
bogdan3107 <olympicra31@gmail.com>
2023-09-22 19:47:12 +0300

    user_popup added

--- app/main/routes.py
+++ app/main/routes.py
@@ -165,3 +165,10 @@ def search():
         posts = []
 
     return render_template('search.html', title=_('Search'), posts=posts, form=g.search_form)
+
+
+@bp.route('/user/<username>/popup')
+@login_required
+def user_popup(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template('user_popup.html', user=user)
